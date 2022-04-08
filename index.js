d2f5c1a1f47a8cccebabaa47818bf7c13d65dca8
Xavier Dunkley <xavierxv18@gmail.com>
2022-04-07 22:42:42 -0400

    test

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function createEmployeeRecord(array){
+    return {
+        firstName: array[0] ,
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents :[],
+        timeOutEvents:[] 
+    }
+
+}
+
+function createEmployeeRecords(arrays){
+
+    console.log(arrays)
+    for(let i=0;i<arrays.length;i++){
+        createEmployeeRecord(arrays[i])
+    }
+}
